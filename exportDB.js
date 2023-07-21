dce74183f45d8ec8a119684de20f9eb661c56d2c
charan379 <37008444+charan379@users.noreply.github.com>
2023-07-21 19:58:34 +0530

    remove creds

--- exportDB.js
+++ exportDB.js
@@ -3,7 +3,4 @@ const backupDatabaseToZip = require("./backupDatabaseToZip").backupDatabaseToZip
 const connectionString = process.argv[2]; // MongoDB connection string
 const outputZipPath = process.argv[3]; // The path where the zip file will be saved
 
-// const connectionString = 'mongodb+srv://charanteja379:swordFish%40379@cluster1.lwi7smq.mongodb.net/moviebunkers?retryWrites=true&w=majority'; // Replace with your MongoDB connection string
-// const outputZipPath = 'backup.zip'; // The path where the zip file will be saved
-
 backupDatabaseToZip(connectionString, outputZipPath);
